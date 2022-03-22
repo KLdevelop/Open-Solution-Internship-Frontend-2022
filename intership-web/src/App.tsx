d7e462e956f2061ba660e51f9f94e424b734e266
”Drop <drapdrop1@gmail.com>
2022-03-22 20:31:01 +0300

    added router

--- intership-web/src/App.tsx
+++ intership-web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 import { HomePage, CounterPage, TodosPage } from 'Common/Pages';
 import { Navigation } from 'Common/Components/Navigation';
@@ -9,12 +9,14 @@ import s from './App.module.scss';
 export const App: React.FC = () => {
   return (
     <div className={s.app}>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/counter" element={<CounterPage />} />
-        <Route path="/todos" element={<TodosPage />} />
-      </Routes>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/counter" element={<CounterPage />} />
+          <Route path="/todos" element={<TodosPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
