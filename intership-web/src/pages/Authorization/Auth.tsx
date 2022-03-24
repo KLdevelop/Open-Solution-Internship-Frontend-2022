57278dd337e99e3a7502336196c5ac63ac3f1c6c
”Drop <drapdrop1@gmail.com>
2022-03-24 16:01:52 +0300

    laid out

--- intership-web/src/pages/Authorization/Auth.tsx
+++ intership-web/src/pages/Authorization/Auth.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import s from './styled.module.scss';
+import logo from './images/logo.svg';
 
 export const AuthPage: React.FC = () => {
-  return <div>auth page</div>;
+  return (
+    <div className={s.container}>
+      <img src={logo} alt="" className={s.logo} />
+      <h1>Please sign in</h1>
+      <input className={s.login} />
+      <input className={s.login} type="password" />
+      <label className={s.remember}>
+        <input type="checkbox" />
+        Remember me
+      </label>
+      <button type="button" className={s.sign}>
+        Sign in
+      </button>
+      <p className={s.years}>2020-2022</p>
+    </div>
+  );
 };
