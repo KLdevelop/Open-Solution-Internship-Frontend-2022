146bd987e2cb80f43043846ca422a710073e8544
”Drop <drapdrop1@gmail.com>
2022-03-24 21:15:14 +0300

    added login-passsword states

--- intership-web/src/pages/Authorization/Auth.tsx
+++ intership-web/src/pages/Authorization/Auth.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { ChangeEvent, ReactEventHandler, useState } from 'react';
 import s from './styled.module.scss';
 import logo from './images/logo.svg';
 
 export const AuthPage: React.FC = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const onLoginChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
   return (
     <div className={s.container}>
       <img src={logo} alt="" className={s.logo} />
       <h1>Please sign in</h1>
-      <input className={s.login} />
-      <input className={s.login} type="password" />
+      <input className={s.login} value={login} onChange={onLoginChange} />
+      <input className={s.login} type="password" value={password} onChange={onPasswordChange} />
       <label className={s.remember}>
         <input type="checkbox" />
         Remember me
