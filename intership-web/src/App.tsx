2cb640adde0e895807ddf402ccd8a8d0475c10ba
”Drop <drapdrop1@gmail.com>
2022-03-27 17:17:03 +0300

    added check for auth

--- intership-web/src/App.tsx
+++ intership-web/src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 import { HomePage, CounterPage, TodosPage, AuthPage } from 'Common/Pages';
-import { Navigation } from 'Common/Components/Navigation';
-
+import { CheckForAuth } from './components';
 import s from './App.module.scss';
 
 export const App: React.FC = () => {
   return (
     <div className={s.app}>
       <Router>
+        <CheckForAuth />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/counter" element={<CounterPage />} />
