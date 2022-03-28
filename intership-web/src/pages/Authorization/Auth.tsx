4d4a599b4a87f566c542c67db0a4b91dd218a17c
”Drop <drapdrop1@gmail.com>
2022-03-28 22:10:06 +0300

    one function for inputs, fetch rewrited with async/await

--- intership-web/src/pages/Authorization/Auth.tsx
+++ intership-web/src/pages/Authorization/Auth.tsx
@@ -1,24 +1,20 @@
 import React, { ChangeEvent, ReactEventHandler, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setAuth } from 'Src/store/actions';
+import { setAuth } from 'Src/models/actions';
+import { useAppDispatch } from 'Src/hooks';
 import s from './styled.module.scss';
 import logo from './images/logo.svg';
 
 export const AuthPage: React.FC = () => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
+  const [authData, setAuthData] = useState({ login: '', password: '' });
   const [checked, setChecked] = useState(false);
   const [isFail, setIsFail] = useState(false);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
-  const onLoginChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLogin(e.target.value);
-  };
-
-  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    authData[e.target.name] = e.target.value;
+    setAuthData({ ...authData });
   };
 
   const onCheck: ReactEventHandler = () => {
@@ -29,8 +25,8 @@ export const AuthPage: React.FC = () => {
     setIsFail(!isLogin);
     if (isLogin === true) {
       setIsFail(false);
-      dispatch(setAuth(isLogin, login));
-      if (checked) localStorage.setItem('authed', login);
+      dispatch(setAuth({ isLogin, login: authData.login }));
+      if (checked) localStorage.setItem('authed', authData.login);
       navigate('/');
     } else {
       setIsFail(true);
@@ -40,38 +36,41 @@ export const AuthPage: React.FC = () => {
   const doAuth = () => {
     const fields = {
       loginData: {
-        login,
-        password,
+        login: authData.login,
+        password: authData.password,
       },
     };
-    fetch('http://127.0.0.1:8080/authorize', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(fields),
-    })
-      .then((resp) => resp.json())
-      .then((r: { isLogin: boolean }) => checkLogin(r.isLogin))
-      .catch((error) => console.log(error));
-  };
-
-  const onSignClick: ReactEventHandler = () => {
-    doAuth();
+    (async () => {
+      const resp = await fetch('http://127.0.0.1:8080/authorize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(fields),
+      });
+      const r: { isLogin: boolean } = await resp.json();
+      checkLogin(r.isLogin);
+    })().catch(() => checkLogin(false));
   };
 
   return (
     <div className={s.container}>
       <img src={logo} alt="" className={s.logo} />
       <h1>Please sign in</h1>
-      <input className={s.login} value={login} onChange={onLoginChange} />
-      <input className={s.login} type="password" value={password} onChange={onPasswordChange} />
+      <input name="login" className={s.login} value={authData.login} onChange={onInputChange} />
+      <input
+        name="password"
+        className={s.login}
+        type="password"
+        value={authData.password}
+        onChange={onInputChange}
+      />
       <label className={s.remember}>
         <input type="checkbox" checked={checked} onChange={onCheck} />
         Remember me
       </label>
       {isFail && <div className={s.error}>login details error try again</div>}
-      <button type="button" className={s.sign} onClick={onSignClick}>
+      <button type="button" className={s.sign} onClick={doAuth}>
         Sign in
       </button>
       <p className={s.years}>2020-2022</p>
