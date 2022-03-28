4ddb10e892401886934b225b1cbad8e80a0166ca
”Drop <drapdrop1@gmail.com>
2022-03-28 22:04:51 +0300

    returned to its original

--- intership-web/src/hooks/index.ts
+++ intership-web/src/hooks/index.ts
@@ -1,7 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from 'Common/Models/store';
-import { RootState as RState } from 'Src/store/reducers';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useTypedSelector: TypedUseSelectorHook<RState> = useSelector;
