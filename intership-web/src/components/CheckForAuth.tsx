7915671d8c71844b0a38d698cc00cd9edad7d09d
”Drop <drapdrop1@gmail.com>
2022-03-30 20:47:08 +0300

    to protect child

--- intership-web/src/components/CheckForAuth.tsx
+++ intership-web/src/components/CheckForAuth.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useTypedSelector } from 'Src/hooks';
-import { setAuth } from 'Src/store/actions';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from 'Src/hooks';
 
-export const CheckForAuth: React.FC = () => {
-  const { isLogin } = useTypedSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!isLogin) {
-      const login = localStorage.getItem('authed');
-      if (login === null) navigate('/auth');
-      else dispatch(setAuth(true, login));
-    }
-  }, [isLogin, dispatch, navigate]);
-
-  return <div />;
-};
+export function CheckForAuth({ children }: { children: JSX.Element }) {
+  const { isLogin } = useAppSelector((state) => state.auth);
+  if (!isLogin) return <Navigate to="/auth" />;
+  return children;
+}
