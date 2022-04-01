dd8a801094e2f6a3cadb44bb9f9debdef26131e0
”Drop <drapdrop1@gmail.com>
2022-04-01 18:13:36 +0300

    deleted

--- intership-web/src/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { authReducer } from './authReducer';
-
-export const rootReducer = combineReducers({
-  auth: authReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
