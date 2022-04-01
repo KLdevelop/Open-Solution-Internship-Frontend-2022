f9a3485561153bcf30fd802aef99e7564a6b03d8
”Drop <drapdrop1@gmail.com>
2022-04-01 18:51:06 +0300

    new page added to export

--- intership-web/src/pages/index.ts
+++ intership-web/src/pages/index.ts
@@ -2,3 +2,4 @@ export { HomePage } from './Home';
 export { CounterPage } from './Counter';
 export { TodosPage } from './Todos';
 export { AuthPage } from './Authorization';
+export { OrganizationsPage } from './Organizations';
