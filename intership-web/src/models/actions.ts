a3ff21a6b38a0809cef246dedff3fc164c33e864
”Drop <drapdrop1@gmail.com>
2022-04-07 19:44:54 +0300

    new actions

--- intership-web/src/models/actions.ts
+++ intership-web/src/models/actions.ts
@@ -2,3 +2,13 @@ import { createAction } from '@reduxjs/toolkit';
 
 export const setAuth =
   createAction<{ isLogin: boolean; login: string; remember: boolean | undefined }>('SET_AUTH');
+
+export interface Organization {
+  id: number;
+  name: string;
+  address: string;
+  INN: number;
+}
+
+export const setOrganizations = createAction<Organization[]>('SET_ORGANIZATIONS');
+export const fetchOrganizations = createAction('FETCH_ORGANIZATIONS');
