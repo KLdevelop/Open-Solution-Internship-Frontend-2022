0257c184891ae98bf0dd1d15d2842a7ac3c7428b
”Drop <drapdrop1@gmail.com>
2022-04-07 22:44:11 +0300

    svg images

---
+++ intership-web/src/pages/Organizations/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as arrowBack } from './arrow/arrow.svg';
+export { default as addBttn } from './plus/plus.svg';
+export { default as arrowTo } from './actions/arrow-right.svg';
+export { default as deleteBttn } from './actions/delete.svg';
+export { default as editBttn } from './actions/pencil.svg';
