4a00f41180410dd9d3b5bd4013b76c21c366ebf5
”Drop <drapdrop1@gmail.com>
2022-04-07 19:45:32 +0300

    added reducer to store

--- intership-web/src/models/store.ts
+++ intership-web/src/models/store.ts
@@ -1,16 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import { authReducer } from 'Src/models/reducers';
-import getUserSaga from './example/sagas';
+import { authReducer, organizationsReducer } from 'Src/models/reducers';
+import rootSaga from './sagas';
 
 const saga = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    organizations: organizationsReducer,
   },
   middleware: [saga],
 });
-saga.run(getUserSaga);
+saga.run(rootSaga);
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
