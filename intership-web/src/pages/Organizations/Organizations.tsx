214a6f988a3e3181d8548b7260e95feb8d486786
”Drop <drapdrop1@gmail.com>
2022-04-13 22:20:13 +0300

    new functional

--- intership-web/src/pages/Organizations/Organizations.tsx
+++ intership-web/src/pages/Organizations/Organizations.tsx
@@ -1,28 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from 'Src/hooks';
-import { fetchOrganizations, Organization } from 'Src/models/actions';
+import { addToDeleteOrg, fetchOrganizations, Organization } from 'Src/models/actions';
 import { Header } from './components/Header';
 import { arrowBack, addBttn, arrowTo, deleteBttn, editBttn } from './assets';
+import { AddOrganizationModal, RedOrganizationModal, AcceptModal } from './components/Modals';
 import s from './style.module.scss';
 
 export const OrganizationsPage: React.FC = () => {
   const dispath = useAppDispatch();
-  const organizations: Organization[] = useAppSelector(
-    (state) => state.organizations.organizations,
+  const { organizations, isLoaded } = useAppSelector((state) => state.organizations);
+  const { deleteProcesses, postProcesses, editProcesses } = useAppSelector(
+    (state) => state.inProcess,
   );
+  const { deleteArr } = useAppSelector((state) => state.inProcess);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isRedModalOpen, setIsRedModalOpen] = useState(false);
+  const [isAcceptModalOpen, setIsAcceptModalOpen] = useState(false);
+  const someOrg: Organization = { id: 0, name: '', address: '', INN: 0 };
+  const [curOrg, setCurOrg] = useState(someOrg);
+  const [curId, setCurId] = useState(0);
+  const onAddClick = () => {
+    setIsAddModalOpen(true);
+  };
+  const onRedClick = (org: Organization) => {
+    setCurOrg(org);
+    setIsRedModalOpen(true);
+  };
+  const onDeleteClick = (id: number) => {
+    setCurId(id);
+    setIsAcceptModalOpen(true);
+  };
+  const deleteOrg = () => {
+    dispath(addToDeleteOrg(curId));
+  };
   useEffect(() => {
     document.title = 'Organizations';
-    dispath(fetchOrganizations());
-  }, [dispath]);
+    if (!isLoaded) dispath(fetchOrganizations());
+  }, [dispath, isLoaded]);
   return (
-    <div className={s.orgscontainer}>
+    <div className={s.orgscontainer} id="orgspage">
+      {isAddModalOpen && (
+        <AddOrganizationModal
+          isOpen={isAddModalOpen}
+          setIsOpen={setIsAddModalOpen}
+          contId="#orgspage"
+        />
+      )}
+      {isRedModalOpen && (
+        <RedOrganizationModal
+          isOpen={isRedModalOpen}
+          setIsOpen={setIsRedModalOpen}
+          contId="#orgspage"
+          org={curOrg}
+        />
+      )}
+      {isAcceptModalOpen && (
+        <AcceptModal
+          isOpen={isAcceptModalOpen}
+          setIsOpen={setIsAcceptModalOpen}
+          contId="#orgspage"
+          action={deleteOrg}
+        />
+      )}
       <Header />
       <div className={s.buttons}>
         <button type="button" className={s.back}>
           <img src={arrowBack} alt="" />
           Back
         </button>
-        <button type="button" className={s.add}>
+        <button type="button" className={s.add} onClick={onAddClick}>
           <img src={addBttn} alt="" />
           Add Organization
         </button>
@@ -36,19 +82,32 @@ export const OrganizationsPage: React.FC = () => {
           <p>Actions</p>
         </div>
         {organizations.map((org) => (
-          <div key={org.id}>
+          <div key={org.id} className={deleteArr.includes(org.id) ? s.toDelete : ''}>
             <p>{org.id}</p>
             <p>{org.name}</p>
             <p>{org.address}</p>
             <p>{org.INN}</p>
             <p>
-              <img src={arrowTo} alt="" />
-              <img src={editBttn} alt="" />
-              <img src={deleteBttn} alt="" />
+              <button type="button">
+                <img src={arrowTo} alt="" />
+              </button>
+              <button type="button" onClick={() => onRedClick(org)}>
+                <img src={editBttn} alt="" />
+              </button>
+              <button onClick={() => onDeleteClick(org.id)} type="button">
+                <img src={deleteBttn} alt="" />
+              </button>
             </p>
           </div>
         ))}
       </div>
+      {postProcesses + deleteProcesses + editProcesses > 0 && (
+        <div className={s.inProcess}>
+          {postProcesses > 0 && <p>{`Posting posts: ${postProcesses}`}</p>}
+          {deleteProcesses > 0 && <p>{`Deleting posts: ${deleteProcesses}`}</p>}
+          {editProcesses > 0 && <p>{`Editing posts: ${editProcesses}`}</p>}
+        </div>
+      )}
     </div>
   );
 };
