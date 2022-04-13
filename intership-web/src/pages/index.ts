7f4f82161fb50914f512dc5e447ecc45e5a90120
”Drop <drapdrop1@gmail.com>
2022-04-13 22:22:50 +0300

    deleted example pages

--- intership-web/src/pages/index.ts
+++ intership-web/src/pages/index.ts
@@ -1,5 +1,2 @@
-export { HomePage } from './Home';
-export { CounterPage } from './Counter';
-export { TodosPage } from './Todos';
 export { AuthPage } from './Authorization';
 export { OrganizationsPage } from './Organizations';
