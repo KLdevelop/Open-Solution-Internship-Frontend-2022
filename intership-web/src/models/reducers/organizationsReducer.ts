53f2efc59f77b74f01772ce72d7cd1db0c49c196
”Drop <drapdrop1@gmail.com>
2022-04-13 22:12:17 +0300

    new cases

--- intership-web/src/models/reducers/organizationsReducer.ts
+++ intership-web/src/models/reducers/organizationsReducer.ts
@@ -1,13 +1,40 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { Organization, setOrganizations } from '../actions';
+import {
+  addOrganization,
+  Organization,
+  redOrganization,
+  removeOrganization,
+  setOrganizations,
+} from '../actions';
 
-const initialState: { organizations: Organization[] } = {
+const initialState: { organizations: Organization[]; isLoaded: boolean } = {
   organizations: [],
+  isLoaded: false,
 };
 
 export const organizationsReducer = createReducer(initialState, (builder) => {
-  builder.addCase(setOrganizations, (state, action) => ({
-    ...state,
-    organizations: action.payload,
-  }));
+  builder
+    .addCase(setOrganizations, (state, action) => ({
+      ...state,
+      organizations: action.payload.organizations,
+      isLoaded: action.payload.isLoaded,
+    }))
+    .addCase(addOrganization, (state, action) => ({
+      ...state,
+      organizations: [...state.organizations, action.payload],
+    }))
+    .addCase(removeOrganization, (state, action) => {
+      const id = action.payload;
+      const ind = state.organizations.findIndex((el) => el.id === id);
+      state.organizations.splice(ind, 1);
+    })
+    .addCase(redOrganization, (state, action) => {
+      const newOrgs = [...state.organizations];
+      const ind = newOrgs.findIndex((org) => org.id === action.payload.id);
+      newOrgs[ind] = action.payload;
+      return {
+        ...state,
+        organizations: newOrgs,
+      };
+    });
 });
