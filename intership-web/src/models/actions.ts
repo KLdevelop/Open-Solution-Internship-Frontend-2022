2d8eb320afcb4996d10691933f6f0ba2ace70a56
”Drop <drapdrop1@gmail.com>
2022-04-13 22:00:54 +0300

    added actions for post, delete and edit organizations

--- intership-web/src/models/actions.ts
+++ intership-web/src/models/actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const setAuth =
-  createAction<{ isLogin: boolean; login: string; remember: boolean | undefined }>('SET_AUTH');
+  createAction<{ isLogin: boolean; login: string | null; remember?: boolean }>('SET_AUTH');
 
 export interface Organization {
   id: number;
@@ -10,5 +10,28 @@ export interface Organization {
   INN: number;
 }
 
-export const setOrganizations = createAction<Organization[]>('SET_ORGANIZATIONS');
+export interface fetchedOrganizations {
+  organizations: Organization[];
+  isLoaded: boolean;
+}
+
+export const setOrganizations = createAction<fetchedOrganizations>('SET_ORGANIZATIONS');
 export const fetchOrganizations = createAction('FETCH_ORGANIZATIONS');
+
+export const addOrganization = createAction<Organization>('ADD_ORGANIZATION');
+export const postOrganization = createAction<Organization>('POST_ORGANIZATION');
+export const incPostOrgProcesses = createAction('INC_POST_PROCESSES');
+export const decPostOrgProcesses = createAction('DEC_POST_PROCESSES');
+export const addToPostOrg = createAction<Organization>('ADD_TO_POST_ORG');
+
+export const removeOrganization = createAction<number>('REMOVE_ORGANIZATION');
+export const deleteOrganization = createAction<number>('DELETE_ORGANIZATION');
+export const incDeleteOrgProcesses = createAction<number>('INC_DELETE_PROCESSES');
+export const decDeleteOrgProcesses = createAction<number>('DEC_DELETE_PROCESSES');
+export const addToDeleteOrg = createAction<number>('ADD_TO_DELETE_ORG');
+
+export const editOrganization = createAction<Organization>('EDIT_ORGANIZATION');
+export const redOrganization = createAction<Organization>('RED_ORGANIZATION');
+export const addToEditOrg = createAction<Organization>('ADD_TO_EDIT_ORG');
+export const incEditOrgProcesses = createAction('INC_EDIT_PROCESSES');
+export const decEditOrgProcesses = createAction('DEC_EDIT_PROCESSES');
