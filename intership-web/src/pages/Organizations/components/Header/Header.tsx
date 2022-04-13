b492395b501bbc338177daafa33af24d7f393da5
”Drop <drapdrop1@gmail.com>
2022-04-13 22:13:50 +0300

    added logout

--- intership-web/src/pages/Organizations/components/Header/Header.tsx
+++ intership-web/src/pages/Organizations/components/Header/Header.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from 'Src/assets/logo/logo.svg';
+import { useAppDispatch } from 'Src/hooks';
+import { setAuth } from 'Src/models/actions';
 import s from './style.module.scss';
 
 export const Header: React.FC = () => {
+  const dispath = useAppDispatch();
+  const onLogoutClick = () => {
+    dispath(setAuth({ isLogin: false, login: null }));
+  };
   return (
     <div className={s.header}>
       <img src={logo} alt="" className={s.logo} />
+      <button type="button" className={s.logout} onClick={onLogoutClick}>
+        Logout
+      </button>
     </div>
   );
 };
