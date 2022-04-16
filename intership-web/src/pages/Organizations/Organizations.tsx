ca1af66c049d4181e2ae738c1642726ae9c105e1
”Drop <drapdrop1@gmail.com>
2022-04-16 17:26:01 +0300

    table is component now

--- intership-web/src/pages/Organizations/Organizations.tsx
+++ intership-web/src/pages/Organizations/Organizations.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from 'Src/hooks';
 import { addToDeleteOrg, fetchOrganizations, Organization } from 'Src/models/actions';
 import { Header } from './components/Header';
-import { arrowBack, addBttn, arrowTo, deleteBttn, editBttn } from './assets';
 import { AddOrganizationModal, RedOrganizationModal, AcceptModal } from './components/Modals';
+import { Table } from './components/Table';
+import { arrowBack, addBttn } from './assets';
 import s from './style.module.scss';
 
 export const OrganizationsPage: React.FC = () => {
   const dispath = useAppDispatch();
   const { organizations, isLoaded } = useAppSelector((state) => state.organizations);
-  const { deleteProcesses, postProcesses, editProcesses } = useAppSelector(
-    (state) => state.inProcess,
+  const { deleteProcesses, postProcesses, editProcesses, deleteArr } = useAppSelector(
+    (state) => state.orgInProcess,
   );
-  const { deleteArr } = useAppSelector((state) => state.inProcess);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isRedModalOpen, setIsRedModalOpen] = useState(false);
   const [isAcceptModalOpen, setIsAcceptModalOpen] = useState(false);
@@ -38,27 +38,17 @@ export const OrganizationsPage: React.FC = () => {
     if (!isLoaded) dispath(fetchOrganizations());
   }, [dispath, isLoaded]);
   return (
-    <div className={s.orgscontainer} id="orgspage">
+    <div className={s.orgscontainer}>
       {isAddModalOpen && (
-        <AddOrganizationModal
-          isOpen={isAddModalOpen}
-          setIsOpen={setIsAddModalOpen}
-          contId="#orgspage"
-        />
+        <AddOrganizationModal isOpen={isAddModalOpen} setIsOpen={setIsAddModalOpen} />
       )}
       {isRedModalOpen && (
-        <RedOrganizationModal
-          isOpen={isRedModalOpen}
-          setIsOpen={setIsRedModalOpen}
-          contId="#orgspage"
-          org={curOrg}
-        />
+        <RedOrganizationModal isOpen={isRedModalOpen} setIsOpen={setIsRedModalOpen} org={curOrg} />
       )}
       {isAcceptModalOpen && (
         <AcceptModal
           isOpen={isAcceptModalOpen}
           setIsOpen={setIsAcceptModalOpen}
-          contId="#orgspage"
           action={deleteOrg}
         />
       )}
@@ -73,34 +63,13 @@ export const OrganizationsPage: React.FC = () => {
           Add Organization
         </button>
       </div>
-      <div className={s.table}>
-        <div className={s.headtable}>
-          <p>id</p>
-          <p>name</p>
-          <p>address</p>
-          <p>INN</p>
-          <p>Actions</p>
-        </div>
-        {organizations.map((org) => (
-          <div key={org.id} className={deleteArr.includes(org.id) ? s.toDelete : ''}>
-            <p>{org.id}</p>
-            <p>{org.name}</p>
-            <p>{org.address}</p>
-            <p>{org.INN}</p>
-            <p>
-              <button type="button">
-                <img src={arrowTo} alt="" />
-              </button>
-              <button type="button" onClick={() => onRedClick(org)}>
-                <img src={editBttn} alt="" />
-              </button>
-              <button onClick={() => onDeleteClick(org.id)} type="button">
-                <img src={deleteBttn} alt="" />
-              </button>
-            </p>
-          </div>
-        ))}
-      </div>
+      <Table
+        tablehead={['id', 'name', 'address', 'INN', 'Actions']}
+        tablelist={organizations}
+        deleteArr={deleteArr}
+        onRedClick={onRedClick}
+        onDeleteClick={onDeleteClick}
+      />
       {postProcesses + deleteProcesses + editProcesses > 0 && (
         <div className={s.inProcess}>
           {postProcesses > 0 && <p>{`Posting posts: ${postProcesses}`}</p>}
