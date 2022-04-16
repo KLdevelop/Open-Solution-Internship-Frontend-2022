81a4fd3d463a9c3e937a79b35595ff09990653ce
”Drop <drapdrop1@gmail.com>
2022-04-16 17:26:57 +0300

    new routes

--- intership-web/src/App.tsx
+++ intership-web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
 
-import { OrganizationsPage, AuthPage } from 'Common/Pages';
+import { OrganizationsPage, AuthPage, DivisionsPage } from 'Common/Pages';
 import { CheckForAuth } from './components';
 import s from './App.module.scss';
+import { EmployeesPage } from './pages/Organizations';
 
 export const App: React.FC = () => {
   return (
@@ -16,6 +17,22 @@ export const App: React.FC = () => {
             element={
               <CheckForAuth>
                 <OrganizationsPage />
+              </CheckForAuth>
+            }
+          />
+          <Route
+            path="/organizations/:orgid"
+            element={
+              <CheckForAuth>
+                <DivisionsPage />
+              </CheckForAuth>
+            }
+          />
+          <Route
+            path="/organizations/:orgid/:divid"
+            element={
+              <CheckForAuth>
+                <EmployeesPage />
               </CheckForAuth>
             }
           />
