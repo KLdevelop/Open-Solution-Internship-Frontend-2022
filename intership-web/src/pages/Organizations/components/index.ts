bc277de5a337b45ae5a34b5f9d896aa05bd21ac1
”Drop <drapdrop1@gmail.com>
2022-04-17 13:25:37 +0300

    export components

---
+++ intership-web/src/pages/Organizations/components/index.ts
@@ -0,0 +1,12 @@
+export { Header } from './Header';
+export {
+  AddEmployeeModal,
+  AddDivisionModal,
+  AddOrganizationModal,
+  RedEmployeeModal,
+  RedDivisionModal,
+  RedOrganizationModal,
+  AcceptModal,
+} from './Modals';
+export { Table } from './Table';
+export { TrackProcesses } from './TrackProcesses';
