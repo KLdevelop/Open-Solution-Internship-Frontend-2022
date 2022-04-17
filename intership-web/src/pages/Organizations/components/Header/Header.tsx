5823e2ac3f4bdb1da7c1ac05514850b9311107f6
”Drop <drapdrop1@gmail.com>
2022-04-17 15:15:51 +0300

    added titles

--- intership-web/src/pages/Organizations/components/Header/Header.tsx
+++ intership-web/src/pages/Organizations/components/Header/Header.tsx
@@ -4,7 +4,13 @@ import { useAppDispatch } from 'Src/hooks';
 import { setAuth } from 'Src/models/actions';
 import s from './style.module.scss';
 
-export const Header: React.FC = () => {
+interface Props {
+  past?: string;
+  title?: string;
+  next?: string;
+}
+
+export const Header: React.FC<Props> = ({ past, title, next }) => {
   const dispath = useAppDispatch();
   const onLogoutClick = () => {
     dispath(setAuth({ isLogin: false, login: null }));
@@ -12,6 +18,9 @@ export const Header: React.FC = () => {
   return (
     <div className={s.header}>
       <img src={logo} alt="" className={s.logo} />
+      <h1 className={s.past}>{past || ''}</h1>
+      <h1>{title || ''}</h1>
+      <h1>{next || ''}</h1>
       <button type="button" className={s.logout} onClick={onLogoutClick}>
         Logout
       </button>
