924c0c934352a33717bbfd7400da4bc1a4db87d7
”Drop <drapdrop1@gmail.com>
2022-04-17 17:54:53 +0300

    added tooltips

--- intership-web/src/pages/Organizations/components/Table/Table.tsx
+++ intership-web/src/pages/Organizations/components/Table/Table.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { tooltip, OkTooltip } from '@deepdub/react-ok-tooltip';
+import '@deepdub/react-ok-tooltip/dist/tooltip.css';
 import { Division, Employee, Organization } from 'Src/models/actions';
 import { arrowTo, deleteBttn, editBttn } from '../../assets';
 import s from './table.module.scss';
@@ -27,6 +29,7 @@ export const Table: React.FC<Props> = ({
   };
   return (
     <div className={isEmployeesPage ? s.empTable : s.table}>
+      <OkTooltip className={s.tooltip} delay={600} />
       <div className={s.headtable}>
         {tablehead.map((h) => (
           <p key={h}>{h}</p>
@@ -41,14 +44,14 @@ export const Table: React.FC<Props> = ({
           {isEmployeesPage && <p>{el[tablehead[4]]}</p>}
           <p className={s.actions}>
             {!isEmployeesPage && (
-              <button type="button" onClick={() => onNextClick(el.id)}>
+              <button type="button" onClick={() => onNextClick(el.id)} ref={tooltip('more')}>
                 <img src={arrowTo} alt="" />
               </button>
             )}
-            <button type="button" onClick={() => onRedClick(el)}>
+            <button type="button" onClick={() => onRedClick(el)} ref={tooltip('edit')}>
               <img src={editBttn} alt="" />
             </button>
-            <button onClick={() => onDeleteClick(el.id)} type="button">
+            <button onClick={() => onDeleteClick(el.id)} type="button" ref={tooltip('delete')}>
               <img src={deleteBttn} alt="" />
             </button>
           </p>
