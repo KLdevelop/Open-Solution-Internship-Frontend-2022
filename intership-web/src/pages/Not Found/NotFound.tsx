e00d1abb918812534959b0ac1c6595d24ccb461f
”Drop <drapdrop1@gmail.com>
2022-04-18 10:14:02 +0300

    changed document title

--- intership-web/src/pages/Not Found/NotFound.tsx
+++ intership-web/src/pages/Not Found/NotFound.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from 'Src/assets/logo/logo.svg';
 import s from './style.module.scss';
 
 export const NotFoundPage: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Not Found';
+  }, []);
   return (
     <div className={s.notfound}>
       <img src={logo} alt="" />
